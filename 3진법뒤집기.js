381fdebbdb50fcfa115f57b4ff33e5b8d1389541
Jung Yeon Park <juon0805@gmail.com>
2023-03-19 17:00:10 +0900

    Delete 3진법뒤집기.js

--- 3진법뒤집기.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    let n_3 = n.toString(3)
-    let reverse = n_3.split("").reverse().join("")
-    let ans = parseInt(reverse, 3)
-
-    return ans
-}
-
-console.log(solution(45))
