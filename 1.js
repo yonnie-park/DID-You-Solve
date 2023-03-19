894a7faaa9674956892f589a7af867755fc12214
Jung Yeon Park <juon0805@gmail.com>
2023-03-19 17:05:03 +0900

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,27 @@
+function isPrime(num){
+  if(num==1) return false
+  if(num==2 ||num==3) return true
+  if(num%2==0 || num%3==0) return false
+  for(let i=5; i*i<=num; i+=6){
+    if(num%i==0|| num%(i+2)==0) return false
+  }
+  
+  return true
+}
+
+function countPrimeTriplets(numbers) {
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      for (let k = j + 1; k < numbers.length; k++) {
+        const sum = numbers[i] + numbers[j] + numbers[k];
+
+        if (isPrime) {
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+}
